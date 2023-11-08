438c167fd7361aba68da93dd4733e8511f1dac76
David Mulholland <david@futr.ai>
2023-11-08 13:09:46 +0000

    mui autocomplete added google place predictions

--- src/components/pages/search/AutoComplete.tsx
+++ src/components/pages/search/AutoComplete.tsx
@@ -1,50 +1,183 @@
-import { useRef, useEffect, useState } from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
+// import LocationOnIcon from "@mui/icons-material/LocationOn";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import parse from "autosuggest-highlight/parse";
+import { debounce } from "@mui/material/utils";
 
-const AutoComplete = () => {
-  const [value, setValue] = useState<string | null>(null);
-  const [inputValue, setInputValue] = useState("");
-  const options = ["Option 1", "Option 2"];
+// This key was created specifically for the demo in mui.com.
+// You need to create a new one for your application.
+const GOOGLE_MAPS_API_KEY = "AIzaSyCBsdxQvERIzbM2CuT_0ZKJOBaBLaY6i8s";
 
-  const acService = new google.maps.places.AutocompleteService();
-  const predictions = () => {
-    acService.getPlacePredictions(
-      {
-        input: inputValue,
-        types: ["geocode"],
-      },
-      function (predictions, status) {
-        console.log(predictions);
-        console.log(status);
+function loadScript(src: string, position: HTMLElement | null, id: string) {
+  if (!position) {
+    return;
+  }
+
+  const script = document.createElement("script");
+  script.setAttribute("async", "");
+  script.setAttribute("id", id);
+  script.src = src;
+  position.appendChild(script);
+}
+
+const autocompleteService = { current: null };
+
+interface MainTextMatchedSubstrings {
+  offset: number;
+  length: number;
+}
+interface StructuredFormatting {
+  main_text: string;
+  secondary_text: string;
+  main_text_matched_substrings?: readonly MainTextMatchedSubstrings[];
+}
+interface PlaceType {
+  description: string;
+  structured_formatting: StructuredFormatting;
+}
+
+export default function GoogleMaps() {
+  const [value, setValue] = React.useState<PlaceType | null>(null);
+  const [inputValue, setInputValue] = React.useState("");
+  const [options, setOptions] = React.useState<readonly PlaceType[]>([]);
+  const loaded = React.useRef(false);
+
+  console.log(value);
+
+  if (typeof window !== "undefined" && !loaded.current) {
+    if (!document.querySelector("#google-maps")) {
+      loadScript(
+        `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places`,
+        document.querySelector("head"),
+        "google-maps"
+      );
+    }
+
+    loaded.current = true;
+  }
+
+  const fetch = React.useMemo(
+    () =>
+      debounce(
+        (
+          request: { input: string },
+          callback: (results?: readonly PlaceType[]) => void
+        ) => {
+          (autocompleteService.current as any).getPlacePredictions(
+            request,
+            callback
+          );
+        },
+        400
+      ),
+    []
+  );
+
+  React.useEffect(() => {
+    let active = true;
+
+    if (!autocompleteService.current && (window as any).google) {
+      autocompleteService.current = new (
+        window as any
+      ).google.maps.places.AutocompleteService();
+    }
+    if (!autocompleteService.current) {
+      return undefined;
+    }
+
+    if (inputValue === "") {
+      setOptions(value ? [value] : []);
+      return undefined;
+    }
+
+    fetch({ input: inputValue }, (results?: readonly PlaceType[]) => {
+      if (active) {
+        let newOptions: readonly PlaceType[] = [];
+
+        if (value) {
+          newOptions = [value];
+        }
+
+        if (results) {
+          newOptions = [...newOptions, ...results];
+        }
+
+        setOptions(newOptions);
       }
-    );
-  };
+    });
 
-  useEffect(() => {
-    predictions();
-  }, [inputValue]);
+    return () => {
+      active = false;
+    };
+  }, [value, inputValue, fetch]);
 
   return (
-    <div>
-      <div>{`value: ${value !== null ? `'${value}'` : "null"}`}</div>
-      <div>{`inputValue: '${inputValue}'`}</div>
-      <br />
-      <Autocomplete
-        value={value}
-        onChange={(event: any, newValue: string | null) => {
-          setValue(newValue);
-        }}
-        inputValue={inputValue}
-        onInputChange={(event, newInputValue) => {
-          setInputValue(newInputValue);
-        }}
-        id="controllable-states-demo"
-        options={options}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="Controllable" />}
-      />
-    </div>
+    <Autocomplete
+      id="google-map-demo"
+      sx={{ width: 300 }}
+      getOptionLabel={(option) =>
+        typeof option === "string" ? option : option.description
+      }
+      filterOptions={(x) => x}
+      options={options}
+      autoComplete
+      includeInputInList
+      filterSelectedOptions
+      value={value}
+      noOptionsText="No locations"
+      onChange={(event: any, newValue: PlaceType | null) => {
+        setOptions(newValue ? [newValue, ...options] : options);
+        setValue(newValue);
+      }}
+      onInputChange={(event, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
+      renderInput={(params) => (
+        <TextField {...params} label="Add a location" fullWidth />
+      )}
+      renderOption={(props, option) => {
+        const matches =
+          option.structured_formatting.main_text_matched_substrings || [];
+
+        const parts = parse(
+          option.structured_formatting.main_text,
+          matches.map((match: any) => [
+            match.offset,
+            match.offset + match.length,
+          ])
+        );
+
+        return (
+          <li {...props}>
+            <Grid container alignItems="center">
+              <Grid item sx={{ display: "flex", width: 44 }}>
+                {/* <LocationOnIcon sx={{ color: "text.secondary" }} /> */}
+              </Grid>
+              <Grid
+                item
+                sx={{ width: "calc(100% - 44px)", wordWrap: "break-word" }}
+              >
+                {parts.map((part, index) => (
+                  <Box
+                    key={index}
+                    component="span"
+                    sx={{ fontWeight: part.highlight ? "bold" : "regular" }}
+                  >
+                    {part.text}
+                  </Box>
+                ))}
+                <Typography variant="body2" color="text.secondary">
+                  {option.structured_formatting.secondary_text}
+                </Typography>
+              </Grid>
+            </Grid>
+          </li>
+        );
+      }}
+    />
   );
-};
-export default AutoComplete;
+}
