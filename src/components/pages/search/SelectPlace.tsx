83f39c856c827ba950612262ab9d1074422358d8
David Mulholland <david@futr.ai>
2023-11-13 17:37:03 +0000

    basic UI set up for select interest page

--- src/components/pages/search/SelectPlace.tsx
+++ src/components/pages/search/SelectPlace.tsx
@@ -1,10 +1,19 @@
-import { Grid } from "@mui/material";
+import { Grid, useMediaQuery, useTheme } from "@mui/material";
 import AutoCompletePlace from "./AutoCompletePlace";
 import PopularDestinations from "./PopularDestinations";
 
 function SelectPlace() {
+  const theme = useTheme();
+  const notLarge = useMediaQuery(theme.breakpoints.down("lg"));
   return (
-    <Grid container justifyContent={"center"}>
+    <Grid
+      container
+      justifyContent={"center"}
+      sx={{
+        paddingTop: notLarge ? "40vh" : "20vh",
+        paddingBottom: notLarge ? "20vh" : "null",
+      }}
+    >
       <AutoCompletePlace />
       <PopularDestinations />
     </Grid>
