4b0b254f7c78f9ab2b9534eb46314a6aa44dba73
David Mulholland <davidmulholland@Davids-MacBook-Pro.local>
2023-12-14 13:48:17 +0000

    dev added to local url

--- src/helpers/getCurrentEnv.ts
+++ src/helpers/getCurrentEnv.ts
@@ -6,7 +6,7 @@ const appUrls = {
 const apiBaseUrls = {
   dev: "https://nmcshhyqq0.execute-api.eu-west-2.amazonaws.com/dev",
   prod: "https://nmcshhyqq0.execute-api.eu-west-2.amazonaws.com/dev",
-  local: "http://localhost:3000",
+  local: "http://localhost:3000/dev",
 };
 
 // I have some of my backend hosted on AWS lambda and some hosted on a EC2, the reason being is openAi can take more than 30 seconds to respond and lambda has a 30 second timeout.
